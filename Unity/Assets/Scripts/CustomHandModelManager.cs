4a619a4305d2ecaafaac8183114b47d84c0fff94
NGD <jch5201@naver.com>
2018-12-01 04:54:36 +0900

    implement server using Unity.Networking.NetworkServer

--- Unity/Assets/Scripts/CustomHandModelManager.cs
+++ Unity/Assets/Scripts/CustomHandModelManager.cs
@@ -9,6 +9,7 @@
 
 using UnityEngine;
 using UnityEngine.Assertions;
+using UnityEngine.Networking;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.Serialization;
@@ -31,20 +32,76 @@ using UnityEditor;
 /// </summary>
 public class CustomHandModelManager : Leap.Unity.HandModelManager
 {
+    protected NetworkServer server;
+
+    override void Start()
+    {
+        base.Start();
+        server.Listen(32389);
+    }
+
+
     protected override void OnUpdateFrame(Frame frame)
     {
         base.OnUpdateFrame(frame);
-        for (int i=0; i< frame.Hands.Count; i++)
+        SendDataToClient(frame);
+        for (int i = 0; i < frame.Hands.Count; i++)
         {
             Hand curHand = frame.Hands[i];
             if (curHand.IsLeft)
             {
-                Debug.Log("Left Hand: ");
+
+                Debug.Log("Left Hand tip position: " + curHand.Fingers[1].TipPosition.ToString());
             }
             else
             {
                 Debug.Log("Right Hand: ");
             }
+        }
+    }
+
+
+
+    public class HandPositionMessage : MessageBase
+    {
+        bool leftHandExists;
+        Vector leftIndexFingerTip;
+        bool rightHandExists;
+        Vector rightIndexFingerTip;
+
+        HandPositionMessage(Frame frame)
+        {
+            leftHandExists = false;
+            rightHandExists = false;
+            foreach (Hand curHand in frame.Hands)
+            {
+                if (curHand.IsLeft)
+                {
+                    leftHandExists = true;
+                    leftIndexFingerTip = curHand.Fingers[1].TipPosition;
+                }
+                else
+                {
+                    rightHandExists = true;
+                    rightIndexFingerTip = curHand.Fingers[1].TipPosition;
+                }
+            }
+        }
+    }
+
+    public class MyMessageTypes
+    {
+        public static short frame = 1001;
+        public static short connect = 1002;
+    };
+
+    void SendDataToClient(Frame frame)
+    {
+        if(server.connections.Count != 0)
+        {
+            msg = new HandPositionMessage(frame);
+
+            server.sendToAll(MyMessageTypes.frame, msg);
         }
     }
     
