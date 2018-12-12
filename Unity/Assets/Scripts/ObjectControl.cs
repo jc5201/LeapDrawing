d411a3a9fa5dd09b82d0252f1db6d55dc313f975
NGD <jch5201@naver.com>
2018-12-13 03:29:14 +0900

    add image buttons & add scripts

---
+++ Unity/Assets/Scripts/ObjectControl.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectControl : MonoBehaviour {
+
+    private ButtonHandler moveButtonHandler;
+    private ButtonHandler rotateButtonHandler;
+    private ButtonHandler scaleButtonHandler;
+    private CustomHandModelManager handModelManager;
+
+    private GameObject focused;
+
+    // Use this for initialization
+    void Start () {
+        handModelManager = GameObject.Find("HandModels").GetComponent<CustomHandModelManager>();
+        moveButtonHandler = GameObject.Find("MoveImageButton").GetComponent<ButtonHandler>();
+        rotateButtonHandler = GameObject.Find("RotateImageButton").GetComponent<ButtonHandler>();
+        scaleButtonHandler = GameObject.Find("ScaleImageButton").GetComponent<ButtonHandler>();
+        focused = null;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if(focused != null)
+        {
+            if (moveButtonHandler.isPressed)
+            {
+
+            }
+        }
+	}
+
+    public void Focus(GameObject target)
+    {
+        focused = target;
+    }
+}
